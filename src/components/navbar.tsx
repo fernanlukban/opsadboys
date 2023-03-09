c1ff0f2b2e878b0df454815c7e6bb43308ddc2d5
Fernan Lukban <fernanlukban@gmail.co>
2023-03-09 01:59:03 -0800

    added socials to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,13 +3,19 @@ import Link from 'next/link'
 export default function NavBar() {
   return (
 	<>
-		<div className="grid grid-rows-2 bg-indigo-200">
+		<div className="grid grid-rows-2 bg-indigo-200 relative">
 			<Link href="/" className="ml-2 text-xl font-medium hover:font-bold">opsadboys</Link>
-			<div className="flex flex-row gap-x-3 text-slate-500 ml-2">
-				<Link href="/blog" className="hover:underline">blog</Link>
-				<Link href="/recipes" className="hover:underline">recipes</Link>
-				<Link href="/music" className="hover:underline">music</Link>
-				<Link href="/movies" className="hover:underline">movies</Link>
+			<div className="flex flex-row text-slate-500 ml-2 w-full">
+				<div className="flex flex-row gap-x-3">
+					<Link href="/blog" className="hover:underline">blog</Link>
+					<Link href="/recipes" className="hover:underline">recipes</Link>
+					<Link href="/music" className="hover:underline">music</Link>
+					<Link href="/movies" className="hover:underline">movies</Link>
+				</div>
+				<div className="flex flex-row gap-x-3 absolute right-0 mr-2">
+					<Link href="https://twitter.com/opsadboys" className="hover:underline">twitter</Link>
+					<Link href="https://instagram.com/fernanlukban" className="hover:underline">instagram</Link>
+				</div>
 			</div>
 		</div>
 	</>
